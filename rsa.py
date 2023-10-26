94d09b5bc22ad20f6f11034db842ea7ca0993a04
spectruni <84639388+spectruni@users.noreply.github.com>
2023-10-26 14:02:46 +0200

    rsa
    * :sparkles: rsa bien sympa
    
    * :sparkles: rsa sans sympy
    
    * :sparkles: arthur le relou

---
+++ rsa.py
@@ -0,0 +1,73 @@
+def get_ciphered(n: int, e: int, m: int) -> int:
+    c = pow(m, e, n)
+    return (c)
+
+
+def get_q_and_phi_from_p(n: int, p: int) -> (int, int):
+    q = n/p
+    if p*q != n:
+        print("Error, n/p is not an int !!")
+    else:
+        q = int(q)
+        phi_n = (p-1)*(q-1)
+    return (int(q), phi_n)
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = egcd(b % a, a)
+    return (g, x - (b//a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('No modular inverse')
+    return x % m
+
+
+def get_d_from_phi_n(e: int, phi_n: int) -> int:
+    d = modinv(e, phi_n)
+    return d
+
+
+def get_clear_from_ciphered(n: int, c: int, d: int) -> int:
+    m = pow(c, d, n)
+    return m
+
+
+if __name__ == "__main__":
+    n = 4331
+    e = 59
+    m1 = 3158
+    p = 61
+    c2 = 167
+
+    print("\nBase parameters:")
+    print("n={n}".format(n=n))
+    print("e={e}".format(e=e))
+    print("m1={m1}".format(m1=m1))
+    print("p={p}".format(p=p))
+    print("c2={c2}".format(c2=c2))
+
+    print("\nQuestion 1:")
+    c1 = get_ciphered(n, e, m1)
+    print("m1={m1}".format(m1=m1))
+    print("c1={c1}".format(c1=c1))
+
+    print("\nQuestion 2:")
+    (q, phi_n) = get_q_and_phi_from_p(n, p)
+    print("n=p*q={n}".format(n=n))
+    print("p={p}".format(p=p))
+    print("q={q}".format(q=q))
+    print("phi(n)=(p-1)*(q-1)={phi_n}".format(phi_n=phi_n))
+
+    print("\nQuestion 3:")
+    d = get_d_from_phi_n(e, phi_n)
+    print("d={d}".format(d=d))
+
+    print("\nQuestion 4:")
+    m2 = get_clear_from_ciphered(n, c2, d)
+    print("c2={c2}".format(c2=c2))
+    print("m2={m2}".format(m2=m2))
